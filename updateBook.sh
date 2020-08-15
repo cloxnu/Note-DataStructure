17f09fcc9d7486343ef6714b976ef64a7b601567
cloxnu <o18089541818@outlook.com>
2020-08-15 20:13:21 +0800

    Remove gh_pages

--- updateBook.sh
+++ updateBook.sh
@@ -1,4 +1,5 @@
-rm -r gh_pages/
-git subtree add --prefix=gh_pages origin gh-pages
-cp -r _book/ gh_pages/
-git subtree push --prefix=gh_pages origin gh-pages
+rm -r gh_pages/;
+git commit -m "Remove gh_pages";
+git subtree add --prefix=gh_pages origin gh-pages;
+cp -r _book/ gh_pages/;
+git subtree push --prefix=gh_pages origin gh-pages;

6297bfbff5b8303ca0557ab3ae07eea33336881f
cloxnu <o18089541818@outlook.com>
2020-09-20 22:30:38 +0800

    Create 7.9

--- _book/docs/docs/docs/updateBook.sh
+++
@@ -1,9 +0,0 @@
-rm -r gh_pages/;
-git add .;
-git commit -m "Remove gh_pages";
-git subtree add --prefix=gh_pages origin gh-pages;
-rm -r gh_pages/.*;
-cp -r _book/ gh_pages/;
-git add .;
-git commit -m "Create gh_pages";
-git subtree push --prefix=gh_pages origin gh-pages;
